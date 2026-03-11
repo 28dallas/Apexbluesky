3b7a06788bd010f6b1444a4c919925a613ebb1dc
28dallas <your-email@example.com>
2026-03-11 15:25:53 +0300

    add

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -4,7 +4,7 @@ import { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { supabase } from '@/lib/supabase';
-import styles from './Auth.module.css';
+import styles from '../login/Auth.module.css';
 
 export default function SignupPage() {
     const [email, setEmail] = useState('');
